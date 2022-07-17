0c688549404aeaf93535d143698d77f73c69827c
Eric Seidel <github@eseidel.com>
2022-07-17 15:27:09 -0700

    Connect the client and the server!
    It works.  A circle moves between locations and the client draws it.

--- packages/shimmer2_client/lib/main.dart
+++ packages/shimmer2_client/lib/main.dart
@@ -1,115 +1,130 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
+
+void debugRegister(io.Socket socket) {
+  const List events = [
+    'connect',
+    'connect_error',
+    'connect_timeout',
+    'connecting',
+    'disconnect',
+    'error',
+    'reconnect',
+    'reconnect_attempt',
+    'reconnect_failed',
+    'reconnect_error',
+    'reconnecting',
+    'ping',
+    'pong'
+  ];
+  for (var event in events) {
+    socket.on(event, (data) {
+      print(event);
+      print(data);
+    });
+  }
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class ClientEntity {
+  final String id;
+  final int x;
+  final int y;
+  ClientEntity({required this.id, required this.x, required this.y});
 
-  // This widget is the root of your application.
+  ClientEntity.fromJson(Map<String, dynamic> json)
+      : id = json['id'],
+        x = json['x'],
+        y = json['y'];
+}
+
+class ServerConnection {
+  io.Socket socket;
+
+  ServerConnection(Uri uri)
+      :
+        // OptionBuilder stuff shouldn't be needed, but seems to be
+        // for dart:io.
+        socket = io.io(uri.toString(),
+            io.OptionBuilder().setTransports(['websocket']).build()) {
+    socket.on('connect', (_) {
+      print('connect');
+      socket.emit('msg', 'test');
+    });
+    debugRegister(socket);
+    socket.on('event', (data) => print(data));
+    socket.on('disconnect', (_) => print('disconnect'));
+    socket.on('fromServer', (_) => print(_));
+  }
+
+  void onTick(void Function(List<ClientEntity> entities) callback) {
+    socket.on('tick', (data) {
+      var entities = jsonDecode(data)
+          .map<ClientEntity>((json) => ClientEntity.fromJson(json))
+          .toList();
+      callback(entities);
+    });
+  }
+}
+
+void main() {
+  runApp(const MyGame());
+}
+
+class MyGame extends StatefulWidget {
+  const MyGame({Key? key}) : super(key: key);
+
+  @override
+  State<MyGame> createState() => _MyGameState();
+}
+
+class _MyGameState extends State<MyGame> {
+  late ServerConnection connection;
+  List<ClientEntity> entities = [];
+
+  @override
+  void initState() {
+    connection = ServerConnection(Uri.parse('http://localhost:3000'));
+    connection.onTick((entities) {
+      setState(() {
+        this.entities = entities;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        home: Stack(fit: StackFit.expand, children: [
+      CustomPaint(
+        painter: WorldPainter(entities),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+    ]));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class WorldPainter extends CustomPainter {
+  final List<ClientEntity> entities;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
+  WorldPainter(this.entities) : super();
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  void paintEntity(Canvas canvas, ClientEntity entity) {
+    var paint = Paint()..color = Colors.green;
+    var offset = Offset(entity.x.toDouble(), entity.y.toDouble());
+    canvas.drawCircle(offset, 5.0, paint);
   }
 
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+  void paint(Canvas canvas, Size size) {
+    for (var entity in entities) {
+      paintEntity(canvas, entity);
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant WorldPainter oldDelegate) {
+    return true;
   }
 }
--- packages/shimmer2_server/bin/shimmer2_server.dart
+++ packages/shimmer2_server/bin/shimmer2_server.dart
@@ -1,5 +1,99 @@
-import 'package:shimmer2_server/shimmer2_server.dart' as shimmer2_server;
+import 'dart:async';
+import 'dart:convert';
+import 'package:socket_io/socket_io.dart';
 
-void main(List<String> arguments) {
-  print('Hello world: ${shimmer2_server.calculate()}!');
+class IPoint {
+  final int x;
+  final int y;
+  const IPoint(this.x, this.y);
+}
+
+class ISize {
+  final int width;
+  final int height;
+  const ISize(this.width, this.height);
+
+  int xPercent(double percent) => (width * percent).floor();
+  int yPercent(double percent) => (height * percent).floor();
+}
+
+abstract class Entity {
+  final String id;
+  IPoint position;
+  Entity({required this.id, required this.position});
+
+  void tick();
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'x': position.x,
+        'y': position.y,
+      };
+}
+
+class GameMap {
+  final ISize size = ISize(100, 100);
+}
+
+class PathFollower extends Entity {
+  List<IPoint> waypoints;
+  int currentWaypointIndex = 0;
+
+  PathFollower({
+    required super.id,
+    required super.position,
+    required this.waypoints,
+  });
+
+  @override
+  void tick() {
+    currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.length;
+    position = waypoints[currentWaypointIndex];
+  }
+}
+
+class Game {
+  GameMap map = GameMap();
+  List<Entity> entities = [];
+  Game();
+
+  void start() {
+    var size = map.size;
+    entities.add(PathFollower(
+      id: 'circle',
+      position: IPoint(10, 10),
+      waypoints: [
+        IPoint(size.xPercent(.5), size.yPercent(.25)),
+        IPoint(size.xPercent(.75), size.yPercent(.5)),
+        IPoint(size.xPercent(.5), size.yPercent(.75)),
+        IPoint(size.xPercent(.25), size.yPercent(.5)),
+      ],
+    ));
+  }
+
+  void tick() {
+    for (var entity in entities) {
+      entity.tick();
+    }
+  }
+}
+
+void main() {
+  var io = Server();
+  io.on('connection', (client) {
+    print('connection default namespace');
+    client.on('msg', (data) {
+      print('data from default => $data');
+      client.emit('fromServer', "ok");
+    });
+  });
+  io.listen(3000);
+
+  var game = Game();
+  game.start();
+
+  Timer.periodic(const Duration(seconds: 2), (timer) {
+    game.tick();
+    io.sockets.emit("tick", jsonEncode(game.entities));
+  });
 }
